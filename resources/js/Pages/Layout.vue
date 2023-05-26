a3d3282e3cc7ffc2eb8b7a9b8c04ee5b7690c8fb
MichaelCorrea478 <michaelcorrea.eb@gmail.com>
2023-05-26 14:15:45 -0300

    Projeto FinApp - Template AdminLte, Inertia, Vue

---
+++ resources/js/Pages/Layout.vue
@@ -0,0 +1,17 @@
+<template>
+    <main>
+        <header>
+            <!-- <Link href="/">Home</Link>
+            <Link href="/about">About</Link>
+            <Link href="/contact">Contact</Link> -->
+        </header>
+        <article>
+            <slot />
+        </article>
+    </main>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3'
+
+</script>
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Layout from './Layout.vue';
+import { Head } from '@inertiajs/vue3'
+
+defineProps({ name: String })
+</script>
+
+<template>
+  <Layout>
+    <Head title="Welcome" />
+    <h1>Welcome</h1>
+    <p>Hello {{ name }}, welcome to your first Inertia app!</p>
+  </Layout>
+</template>
